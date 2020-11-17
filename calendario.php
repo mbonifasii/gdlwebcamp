cf4f19c2f8c5e189a5a0918f63c80b8694b382c2
mbonifasi2019139 <mbonifasi-2019139@kinal.edu.gt>
2020-11-16 18:06:38 -0600

    Trayendo datos de la DB

--- calendario.php
+++ calendario.php
@@ -8,7 +8,13 @@
         try {
             //code...
             require_once('includes/funciones/bd_conexion.php');
-            $sql = "SELECT * FROM eventos";
+            $sql = "SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, nombre_invitado, apellido_invitado ";
+            $sql .= "FROM eventos ";
+            $sql .= " INNER JOIN categoria_evento ";
+            $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
+            $sql .= " INNER JOIN invitados ";
+            $sql .= " ON eventos.id_inv = invitados.invitado_id ";
+            $sql .= " ORDER BY evento_id ";
             $resultado = $conn->query($sql);
         
         } catch (\Exception $e) {
@@ -20,29 +26,68 @@
 
     <div class="calendario">
         <?php
-            $eventos = $resultado->fetch_assoc();//Imprime los resultados
+            $calendario = array();
 
+            while($eventos = $resultado->fetch_assoc()){
+                //Obtiene la fecha del evento
+                $fecha = $eventos['fecha_evento'];
+
+                $evento = array(
+                    'titulo' => $eventos['nombre_evento'],                    
+                    'fecha' => $eventos['fecha_evento'],
+                    'hora' => $eventos['hora_evento'],
+                    'categoria' => $eventos['cat_evento'],
+                    'icono' => $eventos['icono'],
+                    'invitado' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']
+                );
+                $calendario[$fecha][] = $evento;
+            }            
         ?>
 
-        <pre>
-            <?php var_dump($eventos) ?>
-        </pre>
+        <?php // imprime todos los eventos
+            foreach($calendario as $dia => $lista_eventos){?>
+        
+            <h3>
+                <i class="fa fa-calendar"></i>
+                <?php 
+                    setlocale(LC_TIME, "es_ES.UTF-8");
+                    echo strftime("%A, %d de %B del %Y", strtotime($dia)); 
+                ?>
+            </h3>
+            <?php foreach($lista_eventos as $evento){?>
+                <div class="dia">
+                    <p class="titulo"><?php echo $evento['titulo']; ?></p>
+                    <p class="hora">
+                        <i class="fa fa-clock" aria-hidden="true"></i> 
+                        <?php echo $evento['fecha'] . " " . $evento['hora'];?>
+                    </p>
+                    <p>
+                        <i class="fa <?php echo $evento['icono'];?>" aria-hidden="true">
+                        <?php echo $evento['categoria']; ?>
+                    </p>
+                    <p>
+                        <i class="fa fa-user" aria-hidden="true">
+                        <?php echo $evento['invitado']; ?>
+                    </p>
+                </div>
+            <?php }//fin foreach eventos ?>
+        <?php } //foreach de dias ?>
     </div>
     
     <?php
         $conn->close(); 
     ?>
 
-  </section>
+    </section>
 
-  <?php include_once 'includes/templates/footer.php'; ?>
+    <?php include_once 'includes/templates/footer.php'; ?>
 
-  <script src="js/jquery-1.12.0.min.js"></script>
-  <script src="js/jquery.animateNumber.js"></script>
-  <script src="js/jquery.countdown.js"></script>
-  <script src="js/jquery.lettering.js"></script>
-  <script src="js/lightbox.js"></script>
-  <script src="js/main.js"></script>
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <script src="js/jquery.animateNumber.js"></script>
+    <script src="js/jquery.countdown.js"></script>
+    <script src="js/jquery.lettering.js"></script>
+    <script src="js/lightbox.js"></script>
+    <script src="js/main.js"></script>
 
   <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
   <script>
